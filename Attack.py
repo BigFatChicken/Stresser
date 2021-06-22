e303fec4776e9b24888ff413eceede593e1e1c66
BigFatChicken <86238864+BigFatChicken@users.noreply.github.com>
2021-06-22 23:21:28 +0700

    Update Attack.py

--- Attack.py
+++ Attack.py
@@ -3,5 +3,5 @@ import subprocess
 process1 = subprocess.Popen(["python3", "Meteor.py"])
 process2 = subprocess.Popen(["python3", "Meteor2.py"])
 process3 = subprocess.Popen(["python3", "Meteor3.py"])
-process4 = subprocess.Popen(["python3", "Meteor4.py"])
-process5 = subprocess.Popen(["python3", "Meteor5.py"])
+# process4 = subprocess.Popen(["python3", "Meteor4.py"])
+# process5 = subprocess.Popen(["python3", "Meteor5.py"])

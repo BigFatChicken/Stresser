1382c95b580680bdd11079908f6afdd3723cae02
BigFatChicken <86238864+BigFatChicken@users.noreply.github.com>
2021-06-22 23:08:12 +0700

    Add files via upload
    Releasing Script

---
+++ Attack.py
@@ -0,0 +1,7 @@
+import subprocess
+
+process1 = subprocess.Popen(["python3", "Meteor.py"])
+process2 = subprocess.Popen(["python3", "Meteor2.py"])
+process3 = subprocess.Popen(["python3", "Meteor3.py"])
+process4 = subprocess.Popen(["python3", "Meteor4.py"])
+process5 = subprocess.Popen(["python3", "Meteor5.py"])
